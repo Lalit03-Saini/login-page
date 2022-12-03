2beee6b77dd546ab322b109d58c906e6ca617c22
Lalit03-Saini <lalitdaiya54@gmail.com>
2022-12-03 18:55:30 +0530

    add functioality

--- src/login-page.js
+++ src/login-page.js
@@ -1,9 +1,20 @@
 import "./loging-page.css"
+import LogImage from "./Image/Cat Watching You GIF - Cat Watching You Cartoon - Discover & Share GIFs.gif"
+import LogImage1 from "./Image/Noted Ok Sticker - Noted Ok - Discover & Share GIFs.gif"
+import LogImage2 from "./Image/Line Stickers & Themes.gif"
+
+import { useState } from "react"
 
 const Loging = () => {
-    // const [userImage, setUserImage] = useState();
-    // const [paswordImage, setPasswordImage] = userState(false);
-    
+    const [conter, setconter] = useState(LogImage) 
+     
+    function getchage () {
+        setconter(LogImage1)
+    }
+
+    function getchage1 () {
+        setconter(LogImage2)
+    }
 
     return (
         <div className="container">
@@ -12,14 +23,14 @@ const Loging = () => {
             <div className="body-text">
                 <h2 className="heading">Login</h2>
                 <button type="button" className="login-in-button">Already have an account</button>
-                <div className="change-image"></div>
+                <img className="change-image" src={conter} alt="Log-Image"/>
                 <div className="input-text">
                     <div className="col-sm-2 col-form-label">UserName</div>
-                    <input type="text" className="form-control" id="inputUserName" placeholder="@UserName"></input>
+                    <input type="text" className="form-control" id="inputUserName" placeholder="@UserName" onClick={getchage}></input>
                 </div>
                 <div className="input-text">
                     <div className="col-sm-2 col-form-label">Password</div>
-                    <input type="Password" className="form-control" id="inputPassword" placeholder="Pass****"></input>
+                    <input type="Password" className="form-control" id="inputPassword" placeholder="Pass****" onClick={getchage1}></input>
                 </div>
                 <button type="button" className="sign-in-button">Sign me in</button>
             </div>
